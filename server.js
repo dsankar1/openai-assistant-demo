0e3d18cb9a7c1afc9e2d55b79ce45e949c0fb2c0
dsankar1 <daryan.sankar1@gmail.com>
2024-03-21 16:17:52 -0400

    Server change

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.post("/task", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Task app listening on port ${port}`);
 });
